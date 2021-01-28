d0b4f9e885049e88c0c29b3d209fb840dd03d0ea
roalxDcar <roalmollericona@gmail.com>
2021-01-28 03:06:05 -0400

    .

--- resources/views/product/pdf.blade.php
+++ resources/views/product/pdf.blade.php
@@ -5,5 +5,5 @@
         <div style="text-align: center;"><b>Producto:</b> {{$m->name}}</div>
         <div style="text-align: center;"><b>Codigo:</b> {{$m->code}}</div>
         <div style="text-align: center;"><b>Serial:</b> {{$m->serial}}</div>
-        <img  src="img/model/qr-{{$m->name}}{{$m->id}}.png" width="500" height="500" alt="">
+        <img  src="../img/model/qr-{{$m->name}}{{$m->id}}.png" width="500" height="500" alt="">
 @endforeach
